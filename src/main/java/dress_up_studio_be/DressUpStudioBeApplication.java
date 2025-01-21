3a6f51461ab959c8be05c0f05faac0a5c30d2177
Maciech <maciek.chodacki@wp.pl>
2025-01-21 20:02:27 +0100

    Initial backend commit

---
+++ src/main/java/dress_up_studio_be/DressUpStudioBeApplication.java
@@ -0,0 +1,13 @@
+package dress_up_studio_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DressUpStudioBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DressUpStudioBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dress_up_studio_be/DressUpStudioBeApplicationTests.java
@@ -0,0 +1,13 @@
+package dress_up_studio_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DressUpStudioBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
