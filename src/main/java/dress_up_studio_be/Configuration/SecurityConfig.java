29299bc686c8ebeec54ad545bb4342ba61297e1d
Maciech <maciek.chodacki@wp.pl>
2025-01-25 22:54:58 +0100

    Backend security setup - commit 004

--- src/main/java/dress_up_studio_be/Configuration/SecurityConfig.java
+++ src/main/java/dress_up_studio_be/Configuration/SecurityConfig.java
@@ -1,18 +1,22 @@
 package dress_up_studio_be.Configuration;
 
 import dress_up_studio_be.Users.Services.CustomUserDetailsService;
+import jakarta.servlet.Filter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -20,14 +24,19 @@ public class SecurityConfig {
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
 
+    @Autowired
+    private JwtFilter jwtFilter;
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/users/**").authenticated()
+                        .requestMatchers("/admin/**").authenticated()
                         .anyRequest().permitAll()
                 )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
     }
@@ -38,5 +47,10 @@ public class SecurityConfig {
         provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
         provider.setUserDetailsService(customUserDetailsService);
         return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
--- src/main/java/dress_up_studio_be/Users/UserController.java
+++ src/main/java/dress_up_studio_be/Users/UserController.java
@@ -4,13 +4,17 @@ import dress_up_studio_be.Users.Models.UserRequest;
 import dress_up_studio_be.Users.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -19,31 +23,26 @@ public class UserController {
     @Autowired
     private UserService userService;
     @Autowired
-    AuthenticationProvider authenticationProvider;
+    private AuthenticationManager authenticationManager;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody UserRequest loginRequest) {
-        try {
-            // Authenticate the user directly with AuthenticationProvider
-            Authentication authenticate = authenticationProvider.authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            loginRequest.getUsername(),
-                            loginRequest.getPassword()
-                    )
-            );
-
-            // If authentication is successful, return a response
-            UserDetails userDetails = (UserDetails) authenticate.getPrincipal();
-            return ResponseEntity.ok("User logged in: " + userDetails.getUsername());
-
-        } catch (BadCredentialsException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
-        }
+        String token = userService.verifyUser(loginRequest);
+        Map<String, String> response = new HashMap<>();
+        response.put("token", token);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody UserRequest request) {
         userService.saveUserDocument(request);
         return ResponseEntity.ok("User registered successfully");
+    }
+
+    @GetMapping(value = "/users/panel")
+    public ResponseEntity<Map<String, String>> getUserData() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "This is a secured user data response");
+        return ResponseEntity.ok(response);
     }
 }
