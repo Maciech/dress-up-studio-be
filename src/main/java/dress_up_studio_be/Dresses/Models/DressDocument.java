860e81e70ab5d7bc45660f431b0ac8cd5f70f1ca
Maciech <maciek.chodacki@wp.pl>
2025-01-28 20:28:42 +0100

    Photo upload service - commit 003

--- src/main/java/dress_up_studio_be/Dresses/Models/DressDocument.java
+++ src/main/java/dress_up_studio_be/Dresses/Models/DressDocument.java
@@ -5,12 +5,14 @@ import dress_up_studio_be.Dresses.Enums.SIZE;
 import dress_up_studio_be.Utils.DefaultDatabaseFields;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
